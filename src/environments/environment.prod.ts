9ec5dcd6b60e2ba6844f4da30244e85662e77707
fergcb <fergus.bentley@gmail.com>
2022-08-26 09:54:12 +0100

    Dropped an s

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   AUTH_URL: 'https://sakila-auth.fergcb.uk',
-  API_URL: 'http://sakila-data.fergcb.uk',
+  API_URL: 'https://sakila-data.fergcb.uk',
 }
